ff646fd87ab503268b261d7e11285d245d2059bb
sanyam83 <64148610+sanyam83@users.noreply.github.com>
2020-08-27 23:52:14 +0530

    Update parameter.py

--- crnn/parameter.py
+++ crnn/parameter.py
@@ -1,14 +1,14 @@
-CHAR_VECTOR = "adefghjknqrstwABCDEFGHIJKLMNOPZ0123456789"
+CHAR_VECTOR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 letters = [letter for letter in CHAR_VECTOR]
 
 num_classes = len(letters) + 1
 
-img_w, img_h = 128, 64
+img_w, img_h = 128, 32
 
 # Network parameters
 batch_size = 128
 val_batch_size = 16
 
 downsample_factor = 4
-max_text_len = 8
+max_text_len = 15
